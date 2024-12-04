0bbad3e7f9bde03ab80e37d677bc716ce553ed3e
Eliott Brehin <ebrehin@guardiaschool.fr>
2024-12-04 09:53:08 +0100

    relink CV

--- scripts/script.js
+++ scripts/script.js
@@ -36,19 +36,19 @@ const config = {
             name: "GERARD Maxime",
             subtitle: "First years at Guardia School Cybersecurity",
             image: 'maxime_little.png',
-            url: "#"
+            url: "CV/CV Maxime.html"
         },
         {
             name: "CHAUDHRY Taha",
             subtitle: "First years at Guardia School Cybersecurity",
             image: 'taha_little.png',
-            url: "#"
+            url: "CV/CV Taha.html"
         },
         {
             name: "BREHIN Eliott",
             subtitle: "First years at Guardia School Cybersecurity",
             image: 'eliott_little.png',
-            url: "#"
+            url: "CV/Eliott_CV.html"
         },
         {
             name: "FOREST Mathieu",
